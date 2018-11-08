b9f8da28f927a8c8ab6642b90501cb36932e35cc
Jesús Mesa González <sulimesa@gmail.com>
2018-11-08 18:37:37 +0100

    Añadidos ficheros aplicación

---
+++ hello.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 18:07:18 2018
+
+@author: jesus
+"""
+
+from flask import Flask
+from flask import jsonify
+
+# Inicializar la aplicacion.
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+
+        return(jsonify({"key":"Hello World!"}))
+
+if __name__ == "__main__":
+    
+    app.run()
+    
+    
