ea6d3e654e68014720612541bd1c5aaaff80fea8
Jesús Mesa González <sulimesa@gmail.com>
2018-11-13 13:57:27 +0100

    Cambios

--- test/test.py
+++ test/test.py
@@ -15,7 +15,7 @@ import os,sys,inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
-from .. import app
+import app
 
 class testApp(unittest.TestCase):
     
